45aafd41f89224dad36fd3c2a00d89a5624972a5
Elrey Belmonti and Megan Olson <megan@>
2017-08-21 14:43:58 -0700

    add strikethrough element to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
+// business logic
 
+function Item(item) {
+  this.items = item.split(",");
+}
 
-
-
-
-
+// user interface logic
 $(document).ready(function() {
   $("#toDoForm").submit(function(event) {
-  event.preventDefault();
-  var itemInput = $("input#task").val();
-  $("#toDoList").append("<li><span class='item'>" + itemInput + "</span></li>");
+    event.preventDefault();
+    var itemInput = $("input#task").val();
+
+    var newItems = new Item(itemInput);
+    newItems.items.forEach(function(item){
+    $("#toDoList").append("<li><span class='item'>" + item + "</span></li>");
+    });
+    $("input#task").val("");
+    $("span.item").click(function(){
+      $(this).addClass("strike");
+    });
   });
 });
